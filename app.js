e11f954420c313e9158ff3d76b9954d7a42cbb04
yoel <yoel@macbook-pro.home>
2020-07-23 14:45:09 -0700

    [constructorfunction] add sc2 players

--- app.js
+++ app.js
@@ -42,3 +42,35 @@ console.log(llamaTwo);
 
 console.log(llamaTwo.naughty);
 console.log(llamaTwo.name);
+
+// Constructor Functions
+function User(name) {
+  // this
+  this.name = name;
+
+  // return this
+}
+
+let adam = new User("Adam");
+let pete = new User("Pete");
+
+console.log(adam.name);
+console.log(pete.name);
+
+function SC2Player(name, team, banelingBust) {
+  this.name = name;
+  this.team = team;
+  this.banelingBust = banelingBust;
+}
+
+let TY = new SC2Player("TY", "Afreeca Freecs", false);
+console.log(TY);
+console.log(TY.name);
+
+let Dark = new SC2Player("Dark", "Dragon Phoenix Gaming", true);
+console.log(Dark);
+console.log(Dark.name);
+
+let Scarlett = new SC2Player("Scarlett", "Brave Star Gaming", true);
+console.log(Scarlett);
+console.log(Scarlett.name);
